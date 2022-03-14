fd464d18d30e143ff046582de63cd48b8ad830e8
Aditya Singh <88069954+Adi2102@users.noreply.github.com>
2022-03-14 13:40:50 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 
 //Fill the below details from your firebase account
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: "AIzaSyCWV9BNFrxYlHnsCCz29IjwInt2BOHYgEs",
+  authDomain: "netflix-clone-adi-38814.firebaseapp.com",
+  projectId: "netflix-clone-adi-38814",
+  storageBucket: "netflix-clone-adi-38814.appspot.com",
+  messagingSenderId: "873393925918",
+  appId: "1:873393925918:web:3b00e44e765499d2b1630f"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
